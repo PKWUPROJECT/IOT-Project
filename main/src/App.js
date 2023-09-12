4da95b81d0363f662816d53f9ba4b2624758a52e
Sean Michael <120260830+SeanMichaelOwen@users.noreply.github.com>
2023-09-12 08:06:01 +0800

    Upload File
    Menggunakan ReactJs

---
+++ main/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="login-container">
+        <h1>Login</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
